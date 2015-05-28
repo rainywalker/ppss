d6028646c7fea0e4548ecc680a93ff91fe859f6a
jaelomin <rainywalker@gmail.com>
2015-05-28 15:18:10 +0900

    create file

--- cssRuleSet/ruleSet01.js
+++ cssRuleSet/ruleSet01.js
@@ -2,13 +2,19 @@
  * Created by jaelomin on 15. 5. 18..
  */
 
+var doc = document;
 
-var CssStyle = (function(doc){
-
-    var styleEle = document.createElement("style")
+(function(){
+  addEventListener("DOMContentLoaded",function(){
+    var styleEle = doc.createElement("style")
     doc.getElementsByTagName("head")[0].appendChild(styleEle);
     var sheet = styleEle.sheet || styleEle.styleSheet;
     var ruleSet = sheet.rules || sheet.cssRules;
+    sheet.insertRule("p{color:red}",0);
+    console.log(sheet)
+    var rule = ruleSet[0];
+    rule.style["margin"] = "10px 0";
+    rule.style["padding"] = "10px 0"; 
 
-  
-})(document)
+    })
+})()
